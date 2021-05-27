b8851ce216946c958bff3a86de7bf14cc8f6a7d9
Tetiana Bronitska <tanya.nest87@gmail.com>
2021-05-28 00:55:18 +0300

    Added modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './styles.scss';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 
 import NewsApiService from './js/apiService';
 
 import photoCardTpl from '../templates/photo-card.hbs';
+import openModal from './js/openModal';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import { error, info } from '@pnotify/core';
 
 import getRefs from './js/getRefs';
 
@@ -19,12 +24,14 @@ function searchImages(e) {
   newsApiService.query = e.currentTarget.elements.query.value;
 
   if (newsApiService.query === '') {
-    return alert('Enter something');
+    return error({
+                text: 'Enter something',
+            });
   }
 
     newsApiService.resetPage();
      clearImagesContainer();
-    newsApiService.fetchImages().then(images => {
+  newsApiService.fetchImages().then(images => {
     appendImagesMarkup(images);
      newsApiService.incrementPage();
   });
